a26703b5fa41c16b068b1754f1226d90c1df5b80
Lassalle09 <36510808+Lassalle09@users.noreply.github.com>
2018-02-15 10:49:05 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_LassalleJuan.py
@@ -0,0 +1,10 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Juan Lassalle
+# 15 Feb 2018
+
+total_sales = float(input ("Enter the projectec sales:  "))
+
+profit = total_sales * 0.23
+
+print (" The profit is $", profit)
